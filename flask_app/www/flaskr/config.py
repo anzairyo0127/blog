11e08e6795cefaf8f85fd93cf221f3350493fff0
Ryo Anzai <36684885+anzairyo0127@users.noreply.github.com>
2018-10-01 12:44:26 +0900

    テスト実行時のバグを修正するための文言追加

--- flask_app/www/flaskr/config.py
+++ flask_app/www/flaskr/config.py
@@ -1,5 +1,6 @@
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@server/db'
 # https://qiita.com/atsuo1203/items/94e8a8941fceab321c34
-#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://lion:lion@127.0.0.1/db_blog?charset=utf8'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://lion:lion@127.0.0.1/db_blog?charset=utf8'
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://lion:lion@172.19.0.2/db_blog?charset=utf8'
 SECRET_KEY = '\x10R8K\x94\xf4\xbaE\xea!\xca\xf8#\xa3e\xe6_\xad\xe6\xb5F\x95`\x90'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
