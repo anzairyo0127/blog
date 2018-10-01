f874ac93de2a20e635d2507dc2ec721dcf6ab1e8
Ryo Anzai <36684885+anzairyo0127@users.noreply.github.com>
2018-10-01 12:45:17 +0900

    Pylint対応のため試しにモジュールの説明の追加。

--- flask_app/www/flaskr/__init__.py
+++ flask_app/www/flaskr/__init__.py
@@ -1,3 +1,7 @@
+'''
+flaskr読み込み
+'''
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
--- flask_app/www/flaskr/models.py
+++ flask_app/www/flaskr/models.py
@@ -1,13 +1,18 @@
+"""This is a test program."""
 from datetime import datetime
 
 from flaskr import db
 
 
 class Entry(db.Model):
+    '''
+    MRD
+    '''
     __tablename__ = 'entry'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text)
     text = db.Column(db.Text)
+    name = db.Column(db.Text)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False,
                            default=datetime.now, onupdate=datetime.now)
